a4107a92cf6efeb850518a1a40716ed518256ca4
Cortland Finch <99043321+cortlandfinch@users.noreply.github.com>
2023-11-15 16:34:40 -0600

    add starting files

---
+++ project-art/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-black'>
+        <h1 className='w-full text-3xl font-bold text-[#3d4246]'>
+            LINDSEY FINCH ART
+        </h1>
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
